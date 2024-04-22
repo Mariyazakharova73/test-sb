6689a9f3ea3f492b2d2c620bc45f867c1d44406b
Mariya Zakharova <zakharovamaria73@yandex.ru>
2024-04-22 14:06:10 +0400

    feat: task1

---
+++ task_1/index.js
@@ -0,0 +1,49 @@
+import { encoded, translations } from './data.js';
+
+const getDecryptedData = (data, dictionary) => {
+	return data.map(item => {
+		return Object.fromEntries(
+			Object.entries(item).map(([key, value]) => {
+				if (value === null) {
+					return [key, null];
+				}
+
+				if (
+					key === 'groupId' ||
+					key === 'service' ||
+					key === 'formatSize' ||
+					key === 'ca'
+				) {
+					return [key, value];
+				}
+
+				if (dictionary[value] === undefined) {
+					console.log(`Такого ключа нет в словаре`);
+					return [key, value];
+				}
+
+				return [key, dictionary[value]];
+			})
+		);
+	});
+};
+
+const decoded = getDecryptedData(encoded, translations);
+
+function getUniqueValues(arr) {
+	let result = [];
+	arr.forEach(item => {
+		for (let key in item) {
+			if (item[key] === null || key === 'groupId' || key === 'formatSize') {
+				continue;
+			} else {
+				result = [...result, item[key]];
+			}
+		}
+	});
+	return [...new Set(result)];
+}
+
+console.log(decoded);
+
+console.log(getUniqueValues(encoded));
